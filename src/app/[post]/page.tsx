ea9108970ee7f41defabf305fa7147bc4af31106
Ali Kooshesh <alikooshesh007@gmail.com>
2024-06-27 18:35:23 +0330

    fix/style-of-post

--- src/app/[post]/page.tsx
+++ src/app/[post]/page.tsx
@@ -2,35 +2,35 @@ import { Ipost } from "@/interface/postInterface";
 import { timestampToGre } from "@/utils/dateAndTimeHandlers";
 import { createRandomTimeStamp } from "@/utils/fakeData";
 
-async function getPostData(postId : number) {
-    const res = await fetch (`${process.env.NEXT_PUBLIC_BASE_API_URL}/posts/${postId}`);
-  
-    if (!res.ok) {
-      throw new Error(res.statusText)
-    }
-    
-    return res.json();
-  }
+async function getPostData(postId: number) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/posts/${postId}`);
 
-export default async function PostPage({params} : { params: { post: string } }) {
+    if (!res.ok) {
+        throw new Error(res.statusText)
+    }
+
+    return res.json();
+}
+
+export default async function PostPage({ params }: { params: { post: string } }) {
 
     const postId = +(params.post.split("-").at(-1) ?? 0)
 
-    const postData:Ipost = await getPostData(postId)
+    const postData: Ipost = await getPostData(postId)
 
-    return(
+    return (
         <>
-        <div className="flex flex-col gap-[4px]">
-                    <h1 className="inline-block text-transparent bg-clip-text font-black text-[1.75rem] text-black dark:text-white">
-                        {postData.title}
-                    </h1>
-                    <span className="text-[13px] text-gray-700 dark:text-gray-300">
-                        {timestampToGre(createRandomTimeStamp())}
-                    </span>
-                    <p className="mt-1 text-black dark:text-white">
-                        {postData.body}
-                    </p>
-                </div>
+            <article className="flex flex-col gap-[4px]">
+                <h1 className="inline-block text-transparent bg-clip-text font-black text-[1.75rem] text-black dark:text-white">
+                    {postData.title}
+                </h1>
+                <span className="text-[13px] text-gray-700 dark:text-gray-300">
+                    {timestampToGre(createRandomTimeStamp())}
+                </span>
+                <p className="mt-10 text-black dark:text-white">
+                    {postData.body}
+                </p>
+            </article>
         </>
     )
 }
