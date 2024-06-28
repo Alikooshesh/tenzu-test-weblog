8b9f034a15e74c38aef9f4be41e5b91085dcc007
Ali Kooshesh <alikooshesh007@gmail.com>
2024-06-28 11:59:06 +0330

    faet/send-api-function-to-services-folder

--- src/app/[post]/page.tsx
+++ src/app/[post]/page.tsx
@@ -1,16 +1,6 @@
 import { Ipost } from "@/interface/postInterface";
+import { getPostData } from "@/services/postPage/postPageServices";
 import { timestampToGre } from "@/utils/dateAndTimeHandlers";
-import { createRandomTimeStamp } from "@/utils/fakeData";
-
-async function getPostData(postId: number) {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/posts/${postId}`);
-
-    if (!res.ok) {
-        throw new Error(res.statusText)
-    }
-
-    return res.json();
-}
 
 export default async function PostPage({ params }: { params: { post: string } }) {
 
@@ -25,7 +15,7 @@ export default async function PostPage({ params }: { params: { post: string } })
                     {postData.title}
                 </h1>
                 <span className="text-[13px] text-gray-700 dark:text-gray-300">
-                    {timestampToGre(createRandomTimeStamp())}
+                    {timestampToGre(postData.updated_at)}
                 </span>
                 <p className="mt-10 text-black dark:text-white">
                     {postData.body}
